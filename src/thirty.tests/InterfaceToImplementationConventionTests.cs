55d35340f722349e89ad781246ac7b94fac68f47
Darren Cauthon <darren@cauthon.com>
2011-10-04 06:26:33 -0500

    Start writing first spec, stick in a few overridable static methods for testing.

---
+++ src/thirty.tests/InterfaceToImplementationConventionTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMoq.Helpers;
+using Machine.Specifications;
+
+namespace thirty.tests
+{
+    [Subject(typeof (InterfaceToImplementationConvention))]
+    public class when_no_classes_exist_in_the_assembly : with_automoqer
+    {
+        private Establish context =
+            () =>
+                {
+                };
+    }
+
+    public class InterfaceToImplementationConvention
+    {
+    }
+}
---
+++ src/thirty/StaticMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("thirty.tests")]
+
+namespace thirty
+{
+    internal static class StaticMethods
+    {
+        private static Func<Assembly, IEnumerable<Type>> getTypesFunc
+            = assembly =>
+                  {
+                      try
+                      {
+                          return assembly.GetTypes();
+                      }
+                      catch
+                      {
+                          return new Type[] {};
+                      }
+                  };
+
+        private static readonly Func<Assembly, IEnumerable<Type>> getInterfacesFunc
+            = assembly => getTypesFunc(assembly).Where(x => x.IsInterface);
+
+        private static readonly Func<Assembly, IEnumerable<Type>> getConcreteTypes
+            = assembly => getTypesFunc(assembly).Where(x => x.IsInterface == false && x.IsAbstract == false);
+
+        internal static void SetGetTypesFunc(Func<Assembly, IEnumerable<Type>> func)
+        {
+            getTypesFunc = func;
+        }
+
+        internal static IEnumerable<Type> GetTypes(Assembly assembly)
+        {
+            return getTypesFunc(assembly);
+        }
+
+        internal static IEnumerable<Type> GetInterfaces(Assembly assembly)
+        {
+            return getInterfacesFunc(assembly);
+        }
+
+        internal static IEnumerable<Type> GetConcreteTypes(Assembly assembly)
+        {
+            return getConcreteTypes(assembly);
+        }
+    }
+}
